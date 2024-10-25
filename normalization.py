985485a4092013fc3ede87b0b511d2e969bd5809
aidanobryan <84754533+aidanobryan@users.noreply.github.com>
2024-10-25 02:07:46 -0400

    Update and rename index.py to normalization.py

--- normalization.py
+++ normalization.py
@@ -10,18 +10,18 @@ L = .106E-9
 lower = -1E-8
 upper = 1E-8
 
-# Probability density functions (without A^2):
+# Probability density functions (without normalization A^2):
 def P_bonding(x, y, z):
     return ( 1/sqrt(pi)*a**(-3/2)*exp(-sqrt(x**2 + y**2 + z**2)/a) + 1/sqrt(pi)*a**(-3/2)*exp(-sqrt((x - L)**2 + y**2 + z**2)/a) )**2
 
 def P_antibonding(x, y, z):
     return ( 1/sqrt(pi)*a**(-3/2)*exp(-sqrt(x**2 + y**2 + z**2)/a) - 1/sqrt(pi)*a**(-3/2)*exp(-sqrt((x - L)**2 + y**2 + z**2)/a) )**2
 
-# Compute the integrals with a step size that is 1/1000 of the :
+# Compute the integrals:
 integral_bonding = nquad(P_bonding, [[lower, upper], [lower, upper], [lower, upper]])
-print("Integral of the Bonding State: ")
+print("Integral of the Bonding State's Probability Density Function (without normalization): ")
 print(integral_bonding)
 
 integral_antibonding = nquad(P_antibonding, [[lower, upper], [lower, upper], [lower, upper]])
-print("Integral of the Antibonding State: ") 
+print("Integral of the Antibonding State's Probability Density Function (without normalization): ") 
 print(integral_antibonding)
